9048b08b9138ff75de3c9128d37713f9317ccc53
AswaniKumar Jujjuru <aswani.kumar.jujjuru@aa.com>
2023-04-19 15:07:14 -0500

    changes

---
+++ big numexm1.py
@@ -0,0 +1,20 @@
+#writing a program 3 intiger values and finding biggest number.
+a=int(input("enter a First number:"))
+b=int(input("Enter a second number:"))
+c=int(input("Enter a Third number:"))
+#a=20 b=30 c=15
+big=a
+if(b>big):
+    print("{} biggest number".format(big))
+if(c>big):
+    print("{}bigest number".format(big))
+if(c>b):
+    print("{} bigest number".format(big))
+print("*"*30)
+print("this is the biggest number")
+
+
+      
+
+    
+     
---
+++ evenodd1.py
@@ -0,0 +1,8 @@
+#Accepting a numerical value and deciding weather odd or even..
+n=int(input("Enter a number"))
+if(n%2)==0:
+    print("{0} the number is odd".format(n))
+else:
+    print("{} the number is even".format(n))
+print("-"*30)
+print("Exicute the program")
