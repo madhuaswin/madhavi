6a959323334388cdbbebe2849f254afdb5bad262
AswaniKumar Jujjuru <aswani.kumar.jujjuru@aa.com>
2023-04-24 13:12:11 -0500

    changes

--- calexm1.py
+++ calexm1.py
@@ -1,5 +1,8 @@
 #finding the calculator
 cal=input("WELCOME TO CALCULATOR")
-print("--"*30)
+print("-"*25)
 fnum=int(input("Enter the first number"))
-snum=int(input("Enter the second number")) 
+snum=int(input("Enter the second number"))
+eopr=input("Enter opretation")
+print("{} welcome to calculator".format(cal))
+print("{} enter the first name".format(fnum))
