38336d0f6a4f192949ce49a362850e70adab21dc
AswaniKumar Jujjuru <aswani.kumar.jujjuru@aa.com>
2023-06-03 06:53:06 -0500

    changes

---
+++ helwrld.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    print("val of = {}".format(i))
+    for j in range(1,4):
+         print("\t val of = {}".format (j))
+    else:
+        print("*"*30)
+    
+
+
+    
+    
