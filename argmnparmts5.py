aedc45b96d9540e98196462c24e44edaed5d2be6
AswaniKumar Jujjuru <aswani.kumar.jujjuru@aa.com>
2023-06-19 07:57:59 -0500

    params

---
+++ argmnparmts5.py
@@ -0,0 +1,13 @@
+#paramsargumnt5
+def discollinfo(Rnum,Name,Group,City="HYD",college="Universal"):
+    print("\t{},\t{},\t{},\t{},\t{}".format(Rnum,Name,Group,City,college))
+    
+
+#Main program
+print("="*50)
+print("\tRnum\tName\tGroup\tCity\tcollege")
+print("="*50)
+discollinfo(100,'Asish','csc','Irving')
+discollinfo(200,'shrith','Mpc','Copell')
+discollinfo(300,'Ibbu','cec','Plano')
+discollinfo(400,'Gummy','bipc')
---
+++ argumntsparams4.py
@@ -0,0 +1,10 @@
+#argumtparms4
+def discollinfo(Rmnum,Name,Group,City,college=Universel):
+    print("\t{} \t{}\t {}\t {}\t{}".format(Rmnum, Name, Group,City,college))
+    
+#Main program
+print("="*50)
+print("\tRmnum\tName\tGroup\tCity\tcollege")
+print("="*50)
+discollinfo(100,'Asish','csc','Irving')
+discollinfo(200,'shrith','Mpc','Coppell')
